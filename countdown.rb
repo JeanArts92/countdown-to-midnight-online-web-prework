690279d873f02145d2d9fdcca452e4a51f9e0c8d
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-06 11:10:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,12 +7,3 @@ end
 return "HAPPY NEW YEAR!"
 end
 
-#def countdown_with_sleep(num)
-   num = 10   
-  while num > 0
-  sleep 1
-  puts "#{num} SECOND(S) !"
-  num -= 1
-end
-return "HAPPY NEW YEAR!"
-end
