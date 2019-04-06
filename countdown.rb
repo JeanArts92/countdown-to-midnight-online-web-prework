d5e01c4ee9714c8e03e4747719abc710197f6b00
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-06 11:29:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 def countdown(num)
   num = 10    
-  while num >= 0
+  while num >= 1
   puts "#{num} SECOND(S)!"
   num -= 1
 end
@@ -9,7 +9,7 @@ end
 
 def countdown_with_sleep(num)
    num = 10   
-  while num >= 0
+  while num >= 1
   sleep 1
   puts "#{num} SECOND(S) !"
   num -= 1
